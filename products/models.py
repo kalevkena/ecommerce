7099fe2b31c3f2cdaaf09f2657f612ad10885a9c
Kalev Kena <kalevkena@gmail.com>
2020-08-05 22:24:13 +0300

    Updating products/models
    Adding models (Order, OrderItem, ShippingAddress)

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from accounts.models import Customer
 class Category(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True, help_text='Unique value for product page URL.')
@@ -20,7 +20,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=250, unique=True, help_text='Unique value for product page URL, created from name')
-    brand = models.CharField(max_length=50)
+    publisher = models.CharField(max_length=50)
     sku = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     old_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, default=0.00)
@@ -30,8 +30,8 @@ class Product(models.Model):
     is_featured = models.BooleanField(default=False)
     quantity = models.IntegerField()
     description = models.TextField()
-    meta_keywords = models.CharField(max_length=25, help_text='Comma-delimited set of SEO keywords for meta tag')
-    meta_description = models.CharField("Meta Keywords", max_length=255, help_text='Content for description tag')
+    """ meta_keywords = models.CharField(max_length=25, help_text='Comma-delimited set of SEO keywords for meta tag')
+    meta_description = models.CharField("Meta Keywords", max_length=255, help_text='Content for description tag') """
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField(Category)
@@ -53,6 +53,34 @@ class Product(models.Model):
     products = models.ManyToManyField(Product)
 cart = Cart()
 total_amounts_in_cart = cart.products.objects.all.count() """
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class ShippingAddress(models.Model):
+    customer= models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=100, null=True)
+    county = models.CharField(max_length=100, null=True)
+    zipcode = models.CharField(max_length=100, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
+
 
 
 
