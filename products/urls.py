1dfd51a1d5534df032f5fdaf65b710230d176d77
Kalev Kena <kalevkena@gmail.com>
2020-08-09 17:11:09 +0300

    Update urls.py
    Fixing urls

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from products.views import HomeView, ContactView, ProductListView, CreateProductView, cart, checkout
+from products.views import HomeView, ContactView, ProductListView, CreateProductView, DetailProductView, UpdateProductView, DeleteProductView, CartView, checkout
 from . import views
 
 urlpatterns = [
@@ -7,8 +7,11 @@ path('', HomeView.as_view(), name='home'),
 path('contact/', ContactView.as_view(), name='contact'),
 # path('accounts/register/', RegisterView.as_view(), name='register'),
 path('checkout/', views.checkout, name='checkout'), 
-path('product_list/', ProductListView.as_view(), name='product_list'),
+path('list/', ProductListView.as_view(), name='list'),
 path('create/', CreateProductView.as_view(), name='create'),
-path('cart/', views.cart, name='cart')
+path('cart/', CartView.as_view(), name='cart'),
+path('detail/<int:pk>', DetailProductView.as_view(), name='detail'),
+path('update/<int:pk>', UpdateProductView.as_view(), name='update'),
+path('delete/<int:pk>', DeleteProductView.as_view(), name='delete'),
 
 ]
