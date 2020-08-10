b959a8fd858e866aa2bd8b1ca725c56364697d42
Kalev Kena <kalevkena@gmail.com>
2020-08-11 00:15:29 +0300

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,7 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
 
+""" router = routers.DefaultRouter()
+router.register(r'products'. ProductViewSet)
+router.register(r'categories', CategoryViewSet)
+ """
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
@@ -25,3 +30,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')), 
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
