c9048b31d829e85866ccf69bb068c752cfd42489
Kalev Kena <kalevkena@gmail.com>
2020-08-11 00:16:18 +0300

    Update models.py

--- products/models.py
+++ products/models.py
@@ -57,7 +57,7 @@ class Order(models.Model):
     transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
-        return str(self.transaction_id)
+        return str(self.id)
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
