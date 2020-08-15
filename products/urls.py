60d0303bd817159df6abaf807ec6b9875cdb9b6c
Kalev Kena <kalevkena@gmail.com>
2020-08-15 16:44:13 +0300

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from products.views import HomeView, ContactView, ProductListView, CreateProductView, DetailProductView, UpdateProductView, DeleteProductView, CartView, checkout
+from products.views import HomeView, ContactView, ProductListView, CreateProductView, DetailProductView, UpdateProductView, DeleteProductView, CartView, checkout, CategoryView
 from . import views
+from products.views import add_to_cart
+
 
 urlpatterns = [
 path('', HomeView.as_view(), name='home'),
@@ -13,5 +15,7 @@ path('cart/', CartView.as_view(), name='cart'),
 path('detail/<int:pk>', DetailProductView.as_view(), name='detail'),
 path('update/<int:pk>', UpdateProductView.as_view(), name='update'),
 path('delete/<int:pk>', DeleteProductView.as_view(), name='delete'),
+path('category/<str:categories>/', CategoryView, name='category'),
+path('add_to_cart/<int:product_id>', add_to_cart, name='add_to_cart')
 
 ]
