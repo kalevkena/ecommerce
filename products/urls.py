b6493d4bd218e43a2fa3ef7e9ced07529b58675d
Kalev Kena <kalevkena@gmail.com>
2020-08-16 15:20:42 +0300

    Update urls.py
    update urls (checkout and it's success url

--- products/urls.py
+++ products/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
-from products.views import HomeView, ContactView, ProductListView, CreateProductView, DetailProductView, UpdateProductView, DeleteProductView, CartView, checkout, CategoryView
+from products.views import HomeView, ContactView, ProductListView, CreateProductView, DetailProductView, UpdateProductView, DeleteProductView, CartView, CategoryView, PurchaseSuccessView
 from . import views
-from products.views import add_to_cart
+from products.views import add_to_cart, checkout
 
 
 urlpatterns = [
 path('', HomeView.as_view(), name='home'),
 path('contact/', ContactView.as_view(), name='contact'),
 # path('accounts/register/', RegisterView.as_view(), name='register'),
-path('checkout/', views.checkout, name='checkout'), 
 path('list/', ProductListView.as_view(), name='list'),
 path('create/', CreateProductView.as_view(), name='create'),
 path('cart/', CartView.as_view(), name='cart'),
@@ -16,6 +15,9 @@ path('detail/<int:pk>', DetailProductView.as_view(), name='detail'),
 path('update/<int:pk>', UpdateProductView.as_view(), name='update'),
 path('delete/<int:pk>', DeleteProductView.as_view(), name='delete'),
 path('category/<str:categories>/', CategoryView, name='category'),
-path('add_to_cart/<int:product_id>', add_to_cart, name='add_to_cart')
+path('add_to_cart/<int:product_id>', add_to_cart, name='add_to_cart'),
+path('checkout/', views.checkout, name='checkout'), 
+path('purchase_success/', PurchaseSuccessView.as_view(), name='purchase_success'),
+
 
 ]
