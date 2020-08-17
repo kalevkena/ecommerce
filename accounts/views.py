476d947fcd3e223f68d6d1cbc17a5fa1dc3517ef
Kalev Kena <kalevkena@gmail.com>
2020-08-17 23:08:48 +0300

    Update views.py
    Registration form and setting the newly registerd user as a customer

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.http import  HttpResponseRedirect
+from accounts.models import Customer
+
 
 # Create your views here.
 
@@ -11,6 +14,23 @@ class UserRegisterView(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+    def get(self, request, *args, **kwargs):
+        context = {'form': self.form_class()}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            new_user.save()
+            new_custumer = Customer.objects.create(user=new_user)
+            new_custumer.save()
+
+            return HttpResponseRedirect(self.success_url)
+
+        context = {'form': form}
+        return render(request, self.template_name, context)
 
 
 """ def login(request):
