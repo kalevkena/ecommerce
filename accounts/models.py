fed9f5f220f52e4fbbae1ec26b903f56f435476b
Kalev Kena <kalevkena@gmail.com>
2020-08-17 23:10:04 +0300

    Update account/models
    Removing repeated lines

--- accounts/models.py
+++ accounts/models.py
@@ -5,8 +5,6 @@ from django.contrib.auth.models import User
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.user
