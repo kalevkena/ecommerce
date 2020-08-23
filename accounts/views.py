73823167e944cd9e380debaee0c61986678c159f
Kalev Kena <kalevkena@gmail.com>
2020-08-23 16:39:09 +0300

    Update views.py
    update views for adding fields for the registration

--- accounts/views.py
+++ accounts/views.py
@@ -6,12 +6,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.http import  HttpResponseRedirect
 from accounts.models import Customer
-
+from .forms import SignUpForm
 
 # Create your views here.
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
@@ -31,52 +31,3 @@ class UserRegisterView(generic.CreateView):
 
         context = {'form': form}
         return render(request, self.template_name, context)
-
-
-""" def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = auth.authenticate(username=username, password=password)
-
-        if user is not None:
-            auth.login(request, user)
-            return redirect('product_list')
-        else:
-            messages.info(request, 'Invalid credentials')
-            return redirect('login')
-
-
-    else:
-        return render(request, 'login.html')
-
-def register(request):
-
-    if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        username = request.POST['username']
-        email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-
-        if password1==password2:
-            if User.objects.filter(username=username).exists():
-                messages.info(request, 'Username already exists')
-                return redirect('register')
-            elif User.objects.filter(email=email).exists():
-                messages.info(request, 'Email already exists')
-                return redirect('register')
-
-            else:
-                user = User.objects.create(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
-                user.save()
-                print('user created')
-                return redirect('login')
-        else:
-            messages.info(request, 'password is not matching!')
-            return redirect('register')
-        return redirect('/')
-    else: 
-        return render(request, 'register.html') """
